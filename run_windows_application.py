62076187dc9d0d2244298ea7d8b432868e852c3c
tskoczylas <skoczy@gmail.com>
2022-04-19 21:22:48 +0200

    commit123123

--- run_windows_application.py
+++ run_windows_application.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 now = datetime.now()
 add_seconds = 3
 soon = (now + timedelta(seconds=add_seconds)).strftime("%H:%M:%S")
+email_subject = 'HVI TEST 3.0.8 report'
 
 #soon1 = soon.strftime("%H:%M:%S")
 
@@ -16,12 +17,12 @@ def job():
     #Process ID of outlook, can be found in Task Manager / Details
     app = app.connect(process=16412)
     #print([w.window_text() for w in app.windows()])
-    new_email = app.window(title = 'HVI TEST 3.0.8 report' + ' - Message (HTML) ', found_index = 0)
+    new_email = app.window(title = email_subject + ' - Message (HTML) ', found_index = 0)
     new_email.move_window(x=190, y=110, width=1200, height=720, repaint=True)
     new_email.set_focus()
     #new_email.To.send_keys("tomasz.skoczylas@cgi1.com")
-    # new_email.Send.click()
-    new_email.PrintControlIdentifiers()
+    #new_email.Send.click()
+    new_email.print_control_identifiers()
     time.sleep(1)
     return schedule.CancelJob
 
