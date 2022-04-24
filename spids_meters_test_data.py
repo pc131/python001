1843d6d2c02077b88fc6da7a781771a83ec70717
pc131 <skoczy@gmail.com>
2022-04-24 12:00:42 +0200

    spids_meters

--- spids_meters_test_data.py
+++ spids_meters_test_data.py
@@ -1,5 +1,5 @@
 import random
-import pyperclip 
+#import pyperclip 
 import openpyxl as xl
 
 working_dir = "C:\\Users\\skocz\\Downloads\\11\\"
@@ -14,11 +14,14 @@ standalone_spids = wb1.worksheets[1]
 
 def pick_spid_meter_xlsx():
     # how many different SPIDS and METERS to pick from the Excel
-    print("{", end = "")
+    dict_spids = "{"
     for a in range(151,451): #choose SPIDs from rows 150-450
         spid = standalone_spids.cell(row=a, column=1).value
         meter_mnf = standalone_spids.cell(row=a, column=4).value
         meter_ser = standalone_spids.cell(row=a, column=5).value
-        print("'" + str(spid) + "':('" + str(meter_mnf) + "','" + str(meter_ser) + "')", end = "")
-    print("}")
+        dict_spids += "'" + str(spid) + "':('" + str(meter_mnf) + "','" + str(meter_ser) + "'),"
+    dict_spids = dict_spids[:-1] # remove last comma  
+    dict_spids += "}"
+    print(dict_spids)
+    #pyperclip.copy(dict_spids)
 pick_spid_meter_xlsx()
