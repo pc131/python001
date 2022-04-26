5933db269df93a344d8806e5c73340cfe8dbc428
tskoczylas <skoczy@gmail.com>
2022-04-26 15:59:39 +0200

    commit

--- from_business_rules_to_tc_csv_CSV.py
+++ from_business_rules_to_tc_csv_CSV.py
@@ -7,9 +7,9 @@ import shutil
 import os
 
 working_dir = 'C:\\Users\\tomasz.skoczylas\\Downloads\\11\\'
-transaction_name = 'T221.R'
+transaction_name = 'T356.W'
 # EXCEL WITH CORRECT TRANSACTION
-source_test_case = working_dir + 'T353R_T201W_T220W_T221R.xlsx'
+source_test_case = working_dir + 'T355R_T201W_T356W.xlsx'
 transaction_name_simple=transaction_name[1:].replace('.', '')
 folder_suffix = '_RULES_TESTCASES'
 
