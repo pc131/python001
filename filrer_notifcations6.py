39ab54698b40362e222fdbeb20a1e5e1cf83240d
pc131 <skoczy@gmail.com>
2022-04-29 23:40:06 +0200

    commit

--- filrer_notifcations6.py
+++ filrer_notifcations6.py
@@ -1,6 +1,6 @@
 import json
 
-working_dir = 'C:\\Users\\cgi\\Desktop\\_downloads\\11\\'
+working_dir = 'C:\\Users\\skocz\\Downloads\\11\\'
 bad_words = ['---- Start Time', 'Request:', 'Response:', 'Peek Message:']
 
 filename = "TC-C1R-REGRESSION_01_2022-04-27_18-43-43.json"
@@ -23,13 +23,11 @@ print("Started Reading JSON file which contains multiple JSON document")
 with open(source_json_file) as f:
     for jsonObj in f:
         json_message = json.loads(jsonObj)
-        #print("Type of json_message is: " + str(type(json_message)))
-        for MessageContainer in json_message['SendMessageRequest']:
-            for DocumentReferenceNumber in bill['MessageContainer']:
-                print(DocumentReferenceNumber)`
-        # message_container = json_message['SendMessageRequest']['MessageContainer']
-        #print(json_message.get('SendMessageRequest'))
-        #print(next(iter(message_container['Payload']['Transaction'])))
+        print("Type of json_message is: " + str(type(json_message)))
+        #print(json_message)
+        print(json_message['SendMessageRequest']['MessageContainer']['DocumentReferenceNumber'])
+        print(json_message['SendMessageRequest']['MessageContainer']['Payload'])
+        # print(json_message['SendMessageRequest']['MessageContainer']['Payload']['Transaction'][0]['Header'])
         # header = message_container['Payload']['Transaction']
         # print(header)
        # json_messages_list.append(json_message)
@@ -37,3 +35,31 @@ with open(source_json_file) as f:
 # print("Printing each JSON Decoded Object")
 # for json_message in json_messages_list:
 #     print(json_message)
+
+def get_items(test_dict, lvl):
+  
+    # querying for lowest level
+    if lvl == 0:
+        yield from ((key, val) for key, val in test_dict.items()
+                    if not isinstance(val, dict))
+    else:
+  
+        # recur for inner dictionaries
+        yield from ((key1, val1) for val in test_dict.values()
+                    if isinstance(val, dict) for key1, val1 in get_items(val, lvl - 1))
+  
+  
+# initializing dictionary
+test_dict = json_message
+  
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+  
+# initializing K
+K = 2
+  
+# calling function
+res = get_items(test_dict, K)
+  
+# printing result
+print("Required items : " + str(dict(res)))
