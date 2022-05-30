5600e14f2723aaac8c928f457cc9b5d72817634a
tskoczylas <skoczy@gmail.com>
2022-05-30 15:50:51 +0200

    commit

--- spids_meters_test_data.py
+++ spids_meters_test_data.py
@@ -2,8 +2,8 @@ import random
 #import pyperclip 
 import openpyxl as xl
 
-working_dir = "C:\\Users\\skocz\\Downloads\\11\\"
-# working_dir = "C:\\Users\\tomasz.skoczylas\\Downloads\\11\\"
+#working_dir = "C:\\Users\\skocz\\Downloads\\11\\"
+working_dir = "C:\\Users\\tomasz.skoczylas\\Downloads\\11\\"
 spids_meters_filename = working_dir + "TEST_DATA.xlsx"
 wb1 = xl.load_workbook(spids_meters_filename)
 ws11 = wb1.worksheets[0]
@@ -15,7 +15,7 @@ standalone_spids = wb1.worksheets[1]
 def pick_spid_meter_xlsx():
     # how many different SPIDS and METERS to pick from the Excel
     dict_spids = "{"
-    for a in range(151,451): #choose SPIDs from rows 150-450
+    for a in range(51,351): #choose SPIDs from rows 50-350
         spid = standalone_spids.cell(row=a, column=1).value
         meter_mnf = standalone_spids.cell(row=a, column=4).value
         meter_ser = standalone_spids.cell(row=a, column=5).value
